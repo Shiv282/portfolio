d626be76edfa71ebbde5ed5a256112e992ba64b8
Shivaraj <shivucm8055@gmail.com>
2024-04-01 08:36:25 +0530

    Adding generic layout, CSS and root directory page(About)

---
+++ src/app/layout.jsx
@@ -0,0 +1,51 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ProfileCard from './ProfileCard';
+import MainCard from "./MainCard";
+import NavBar from "./navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }) {
+  const polygonStyle = {
+    clipPath:
+      "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+  };
+
+  return (
+    <html lang="en">
+      
+      <body>
+        <div id="main" className="bg-white">
+        <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+        <div className="relative" style={{ left: 'calc(50% - 11rem)', aspectRatio: '1155/678', width: '36.125rem', transform: 'translateX(-50%)', backgroundImage: 'linear-gradient(to top right, #ff80b5, #9089fc)', opacity: '0.3', left: 'calc(50% - 30rem)', width: '72.1875rem' }}>
+          {/* Content */}
+        </div>
+      </div>
+
+      <div className="flex-container">
+      <ProfileCard/>
+      <div className="side-card">
+      <NavBar />
+      <main>{children}</main>
+      </div>
+      </div>
+      
+      
+      
+      
+      <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+        <div className="relative" style={{ left: 'calc(50% + 3rem)', aspectRatio: '1155/678', width: '36.125rem', transform: 'translateX(-50%)', backgroundImage: 'linear-gradient(to top right, #ff80b5, #9089fc)', opacity: '0.3', left: 'calc(50% + 36rem)', width: '72.1875rem' }}>
+          {/* Content */}
+        </div>
+      </div>
+    </div>
+        </div>
+      </body>
+    </html>
+  );
+}
+
+
+
---
+++ src/app/page.jsx
@@ -0,0 +1,174 @@
+import Image from "next/image";
+import WorkIcon from '@mui/icons-material/Work';
+
+export default function Page() {
+  return (
+    <>
+      <div className="main-card">
+        <div>
+          <h1 className="text-3xl font-black text-slate-900">About</h1>
+          <p className="text-slate-900 mt-2 mb-5">
+            As a graduating student passionate about technology, I am eager to
+            leverage my skills and enthusiasm to collaborate with
+            forward-thinking teams. I am seeking opportunities to contribute
+            value and drive innovation in a dynamic and growth-oriented
+            environment. With a deep love for technology and a commitment to
+            continuous learning, I am excited to embark on a rewarding career
+            journey that aligns with my interests and goals.
+          </p>
+        </div>
+
+        <div className="experience w-full">
+            <div className="flex flex-row">
+                <WorkIcon
+                  className="text-3xl"
+                  style={{ fontSize: "32px", stroke: "black" }}
+                />
+                <h3 className=" mx-3 text-2xl font-extrabolded text-slate-900">
+                  Experience
+                </h3>
+              </div>
+
+              <div className="card flex flex-col mt-3 rounded-lg bg-gray-100 px-2 py-2"> 
+                <span className="mx-5 text-xl font-extrabolded text-slate-900">
+                Whatfix
+                </span>
+                <span className="mx-5 text-xl text-slate-900">
+                [Sep 2024 - present]
+                </span>
+                <span className="mx-5 text-xl text-slate-900">
+                During my internship at Whatfix, a leading SaaS company specializing in Digital Adoption Platforms, I delved into understanding how extensions work, explored API structures, learned about script injections, and delved into analytics. This experience provided me with a comprehensive understanding of the inner workings of a SaaS company, including its technology stack and operational processes.
+                </span>
+              </div>
+
+              <div className="card flex flex-col mt-3 rounded-lg bg-gray-100 px-2 py-2"> 
+                <span className="mx-5 text-xl text-slate-900">
+                Farmicon
+                </span>
+                <span className="mx-5 text-xl text-slate-900">
+                [Sep 2022 - Nov 2022]
+                </span>
+                <span className="mx-5 text-xl text-slate-900">
+                Developed a dynamic website fetching real-time commodity prices from local mandis
+across India, integrating weather forecasts and info about various agricultural schemes
+It enhanced my web development skills via collaborating with farmers to create a
+user-friendly platform empowering their agricultural endeavors.
+                </span>
+              </div>
+            </div>
+      </div>
+    </>
+  );
+}
+
+/*export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.js</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore starter templates for Next.js.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  );
+}*/
