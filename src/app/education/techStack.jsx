37c235b82061a65775538159ba662ebf56873aad
Shivaraj <shivucm8055@gmail.com>
2024-04-01 12:13:52 +0530

    Adding Education route

---
+++ src/app/education/techStack.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+
+const TechStackLogos = () => {
+  return (
+    <div style={{ position: 'relative', width: '100%' }}>
+        <div className='imgRow mt-5 flex flex-row items-stretch grid gap-4 grid-cols-5 justify-items-center'>
+        <img
+          src='images/python.png'
+          alt='python'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/sql.png'
+          alt='sql'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/java.png'
+          alt='java'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/html_css_js.png'
+          alt='html_css_js'
+          style={{
+            width: '300px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/git.png'
+          alt='git'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/c.png'
+          alt='c'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/cpp.png'
+          alt='cpp'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/react.png'
+          alt='react'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/typescript.png'
+          alt='typescript'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/nodejs.png'
+          alt='nodejs'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/nextjs.svg'
+          alt='nextjs'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/mongodb.png'
+          alt='mongodb'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src='images/expressjs-icon.svg'
+          alt='expressjs'
+          style={{
+            width: '100px', 
+            height: '100px', 
+            objectFit: 'contain',
+          }}
+        />
+        </div>
+        
+    </div>
+  );
+};
+
+export default TechStackLogos;
